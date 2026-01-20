e6b005ff6f7499e15ed34dae4f16eb67bb0eff6e
k025c0060 <k025c0060@g.neec.ac.jp>
2026-01-20 14:32:21 +0900

    base

---
+++ js/load.js
@@ -0,0 +1,13 @@
+$(function(){
+    // 3000ミリ秒（3秒）後に処理を実行
+    setTimeout(function(){
+      
+      // .load をフェードアウト（消す）
+      $('.load').fadeOut(500, function(){
+        // .loadが消え終わったら .main をフェードイン（表示）
+        $(this).remove();
+        $('.main').fadeIn(500);
+      });
+
+    }, 3000); // ここで待機時間を指定
+});
